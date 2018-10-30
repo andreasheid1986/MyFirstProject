3df4b5ffeddce1ca42de232cb8909aed4603d735
Andreas Heid <a.heid@knoll-feinmechanik.de>
2018-10-30 19:21:43 +0100

    change

--- my_project/settings.py
+++ my_project/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/andreasheid/my_first_project/static'
+STATIC_ROOT = "/home/andreasheid/my_first_project/static"
