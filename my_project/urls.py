7d46e11078bd6aa0c64e0bd31d34ee504982f7b6
Andreas Heid <a.heid@knoll-feinmechanik.de>
2018-10-30 23:23:59 +0100

    kook and feel

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('myfirstapp/', include('my_first_app.urls')),
+    path('', include('my_first_app.urls')),
     path('admin/', admin.site.urls),
 ]
