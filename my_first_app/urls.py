8a0db9c9f5a36cc44642fc5ef4dbab123f4bdee4
Andreas Heid <a.heid@knoll-feinmechanik.de>
2018-11-01 22:06:01 +0100

    new pages added

--- my_first_app/urls.py
+++ my_first_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('more/', views.more, name='more'),
+    path('error_message_app/', views.error_message_app, name='error_message_app'),
 ]
